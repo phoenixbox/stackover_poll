173f4431101c00fe0d69e49b8e7414e225660f76
phoenixbox <shane@quickleft.com>
2013-10-21 21:39:08 -0600

    stack connection initialize method

--- lib/stack_connection.rb
+++ lib/stack_connection.rb
@@ -1,2 +1,9 @@
 class StackConnection
+  attr_reader :client_id, :client_secret, :key
+
+  def initialize(options)
+    @client_id = options[:stack_client_id]
+    @client_secret = options[:stack_client_secret]
+    @key = options[:stack_key]
+  end
 end
--- spec/lib/stack_connection_spec.rb
+++ spec/lib/stack_connection_spec.rb
@@ -1,4 +1,17 @@
 require 'spec_helper'
 
 describe StackConnection do
+  describe '#initialize' do
+    let(:valid_options) {
+      { :stack_client_id => ENV["STACK_CLIENT_ID"], :stack_client_secret => ENV["STACK_CLIENT_SECRET"], :stack_key => ENV["STACK_KEY"] }
+    }
+    context 'with valid options' do
+      it 'creates a valid communicator and exposes attributes' do
+        communicator = StackConnection.new(valid_options)
+        expect(communicator.client_id).to eq ENV["STACK_CLIENT_ID"]
+        expect(communicator.client_secret).to eq ENV["STACK_CLIENT_SECRET"]
+        expect(communicator.key).to eq ENV["STACK_KEY"]
+      end
+    end
+  end
 end
